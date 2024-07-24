37ec1f80d5235f0c4a967be1854260ef292f65cc
21A31A0513 <116904563+21A31A0513@users.noreply.github.com>
2024-07-24 21:44:54 +0530

    Create Valid Palindrome II.py

---
+++ String/Valid Palindrome II.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l,r=0,len(s)-1
+        if s==s[::-1]:
+            return True
+        while l<=r:
+            if s[l]!=s[r]:
+                s1=s[:l]+s[l+1:]
+                s2=s[:r]+s[r+1:]
+                return s1==s1[::-1] or s2==s2[::-1]
+            l+=1
+            r-=1       
